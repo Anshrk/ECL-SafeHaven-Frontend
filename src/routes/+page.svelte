46c6d29b619dda661663142c4297f8add47dcde1
Ansh Kashyap <anshrk.13@gmail.com>
2024-03-13 16:30:46 +0530

    :tada: Started the frontend project
    Created a few pages

---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<!-- <div style="max-width:100%;overflow:hidden;color:red;width:500px;height:500px;"><div id="google-maps-display" style="height:100%; width:100%;max-width:100%;"><iframe title="maps"style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/search?q=33.8757803+-84.7609022999999&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="from-embedmap-code" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="get-map-data">premium bootstrap themes</a><style>#google-maps-display img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}</style></div> -->
+
+				<!-- <source srcset={welcome} type="image/webp" /> -->
+				<!-- <img src={welcome_fallback} alt="Welcome" /> -->
+			</picture>
+		</span>
+
+		<!-- to your new<br />SvelteKit app -->
+	</h1>
+
+	<!-- <h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2> -->
+
+	<!-- <Counter /> -->
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
